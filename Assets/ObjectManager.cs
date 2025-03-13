f0d0e0f24c93575e591d97f2073b58a37e70716f
A01635458 <A01635458@tec.mx>
2025-03-13 03:25:48 -0600

    LISTO

--- Assets/ObjectManager.cs
+++ Assets/ObjectManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class ObjectManager : MonoBehaviour
 {
-
     private List<Reparable> electrodomesticos;
     private int ListSize; 
     private int actualItem;
@@ -12,103 +11,81 @@ public class ObjectManager : MonoBehaviour
     private GameObject tempClone;
     public Vector3 clonePosition = new Vector3(19f, 0.8f, -9.5f); 
     public float cloneScaleFactor = 0.6f; // factor de escala 
-    public float moveDuration = 0.5f; // para q se muevan extraño
-    public float startHeightOffset = -2f;
+    public float moveDuration = 0.5f; // para que se muevan suavemente
+    public float startHeightOffset = -2f; // Altura de bajada
 
-    
-    public void Awake(){
-    
-        
-    }
-    public void Start()
+    private void Start()
     {
-        //El codigo Reparable es donde seria buen lugar para que pongas los efectos
-         electrodomesticos = new List<Reparable>(FindObjectsByType<Reparable>(FindObjectsSortMode.None));
+        electrodomesticos = new List<Reparable>(FindObjectsByType<Reparable>(FindObjectsSortMode.None));
         actualItem = 0;
         ListSize = DataManager.Instance.questionsCount;
-        while(electrodomesticos.Count > ListSize){
-            electrodomesticos.RemoveAt(Random.Range(0,electrodomesticos.Count));
+        
+        while (electrodomesticos.Count > ListSize)
+        {
+            electrodomesticos.RemoveAt(Random.Range(0, electrodomesticos.Count));
         }
+
         Debug.Log(electrodomesticos.Count);
         StartCoroutine(HideObjects());
-        
     }
 
-    public void RepairResult(bool result){
-        if(result){
-            electrodomesticos[actualItem].gameObject.SetActive(true);
-            //Efecto de reparacion
-            StartCoroutine(MoveObject(electrodomesticos[actualItem].gameObject, startHeightOffset, 0));
-
-        }else{
-            //Efecto de danio
+    public void RepairResult(bool result)
+    {
+        if (result)
+        {
+            electrodomesticos[actualItem].MoveUp(); // Subir el objeto original a su posición inicial
         }
-        //destruir copia al contestar preguunta
+        else
+        {
+            // Efecto de daño (Aquí puedes agregar algún efecto visual o de sonido)
+        }
+
+        // Destruir copia (clon) al contestar la pregunta
         if (tempClone != null)
         {
-            StartCoroutine(MoveObject(tempClone, 0, startHeightOffset, true));
-            //Destroy(tempClone);
+            Destroy(tempClone);
         }
-        //Next item
+
+        // Siguiente objeto
         actualItem++;
         StartReparation();
     }
-    private IEnumerator HideObjects(){
-        foreach(Reparable r in electrodomesticos){
-            r.gameObject.SetActive(false);
+
+    private IEnumerator HideObjects()
+    {
+        foreach (Reparable r in electrodomesticos)
+        {
+            r.MoveDown(); // Mueve el objeto hacia abajo
             yield return new WaitForSeconds(0.2f);
         }
     }
-    private IEnumerator ShowItem(){
+
+    private IEnumerator ShowItem()
+    {
         yield return new WaitForSeconds(0.6f);
-        //Aqui seria Movel el item al punto "actualItem"
-        //Le puse un poco de delay para que no sea todo instantaneo
-        // copia temporal del objeto antes de que aparezca
+
         if (actualItem < electrodomesticos.Count)
         {
             tempClone = Instantiate(electrodomesticos[actualItem].gameObject, clonePosition, Quaternion.identity);
-            //que vea para enfrente
             tempClone.transform.rotation = Quaternion.LookRotation(Vector3.forward);
-            //cosillas para asegurarme q se vea
             tempClone.SetActive(true);
-            //scale
             tempClone.transform.localScale = electrodomesticos[actualItem].transform.localScale * cloneScaleFactor;
+
             Renderer rend = tempClone.GetComponent<Renderer>();
             if (rend != null)
             {
-            rend.enabled = true;
+                rend.enabled = true;
             }
 
-            //quitar scripts
             foreach (MonoBehaviour script in tempClone.GetComponents<MonoBehaviour>())
             {
                 script.enabled = false;
             }
-            StartCoroutine(MoveObject(tempClone, startHeightOffset, 0));
         }
     }
 
-    public void StartReparation(){
+    public void StartReparation()
+    {
         StartCoroutine(ShowItem());
     }
-    private IEnumerator MoveObject(GameObject obj, float startOffset, float endOffset, bool destroyAfter = false)
-    {
-        Vector3 startPos = obj.transform.position;
-        Vector3 targetPos = new Vector3(startPos.x, startPos.y + endOffset, startPos.z);
-        float elapsedTime = 0;
-
-        while (elapsedTime < moveDuration)
-        {
-            obj.transform.position = Vector3.Lerp(startPos, targetPos, elapsedTime / moveDuration);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-        obj.transform.position = targetPos;
-        
-        if (destroyAfter)
-        {
-            Destroy(obj); // Destruir clon después de la animación
-        }
-    }
-
 }
--- Assets/Reparable.cs
+++ Assets/Reparable.cs
@@ -1,16 +1,41 @@
+using System.Collections;
 using UnityEngine;
 
 public class Reparable : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Vector3 originalPosition;
+    private float lastMoveOffset; // Guarda cuánto bajó el objeto
+
+    private void Start()
     {
-        
+        originalPosition = transform.position; // Guarda la posición inicial
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator MoveObject(float offset)
     {
-        
+        Vector3 startPos = transform.position;
+        Vector3 targetPos = new Vector3(startPos.x, startPos.y + offset, startPos.z);
+        float elapsedTime = 0;
+        float moveDuration = 0.5f;
+
+        while (elapsedTime < moveDuration)
+        {
+            transform.position = Vector3.Lerp(startPos, targetPos, elapsedTime / moveDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+    }
+
+    public void MoveDown()
+    {
+        lastMoveOffset = -7f; // Baja 5 unidades (o el valor que desees)
+        StartCoroutine(MoveObject(lastMoveOffset));
+    }
+
+    public void MoveUp()
+    {
+        StartCoroutine(MoveObject(-lastMoveOffset)); // Sube la misma cantidad que bajó
     }
 }
